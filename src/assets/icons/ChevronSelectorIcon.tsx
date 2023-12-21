a7b259e34089c00e0840dfca046b06eb2b85800f
Sem Prygorodov <semen_p@inbox.lv>
2023-12-21 14:13:31 +0200

    Added chevron icons

---
+++ src/assets/icons/ChevronSelectorIcon.tsx
@@ -0,0 +1,13 @@
+const ChevronSelectorIcon = () => (
+  <svg width="14px" height="14px" viewBox="0 0 24 24" fill="none">
+    <path
+      d="M7 15L12 20L17 15M7 9L12 4L17 9"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default ChevronSelectorIcon;
--- src/assets/icons/CrossIcon.tsx
+++ src/assets/icons/CrossIcon.tsx
@@ -1,15 +1,13 @@
-const CrossIcon = () => {
-  return (
-    <svg width="12px" height="12px" viewBox="0 -0.5 21 21">
-      <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-        <g transform="translate(-419.000000, -240.000000)" fill="#000000">
-          <g id="icons" transform="translate(56.000000, 160.000000)">
-            <polygon points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"></polygon>
-          </g>
+const CrossIcon = () => (
+  <svg width="12px" height="12px" viewBox="0 -0.5 21 21">
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <g transform="translate(-419.000000, -240.000000)" fill="#000000">
+        <g id="icons" transform="translate(56.000000, 160.000000)">
+          <polygon points="375.0183 90 384 98.554 382.48065 100 373.5 91.446 364.5183 100 363 98.554 371.98065 90 363 81.446 364.5183 80 373.5 88.554 382.48065 80 384 81.446"></polygon>
         </g>
       </g>
-    </svg>
-  );
-};
+    </g>
+  </svg>
+);
 
 export default CrossIcon;
